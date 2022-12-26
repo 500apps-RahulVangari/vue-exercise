5e7a2e3b56308b469666301c2af0448a10f96a48
Manisha-500apps <manisha.tatukuluri@500apps.com>
2022-12-26 20:00:33 +0530

    Quiz App

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,58 @@
 <template>
   <div id="app">
-    <McqQuestions ref="child1" />
-
-    <FillBlanks ref="child2" />
-
-    <input type="submit" value="Submit" />
-    <p>{{ result }}</p>
+    <!--<QuestionBox msg="Welcome to Your QuestionBox"/>
+    <FillBlanks msg="Welcome to Your FillBlanks"/>-->
+    <QuestionBox msg="welcome to quiz" ref="child1"/>
+  <FillBlanks msg="Fill in the blanks" ref="child2"/>
+    <button @click="submit()" :class="btn-submit">submit</button>
+    <p>Result : {{ result }}</p>
+   
   </div>
+  
 </template>
 
 <script>
-import FillBlanks from "./components/FillBlanks.vue";
-
-import McqQuestions from "./components/McqQuestions.vue";
+import FillBlanks from './components/FillBlanks.vue';
+//import HelloWorld from './components/HelloWorld.vue'
+import QuestionBox from './components/QuestionBox.vue';
 
 export default {
-  name: "App",
-
+  name: 'App',
   components: {
-    McqQuestions,
-
-    FillBlanks,
+   
+    QuestionBox,
+    FillBlanks
   },
-  data() {
-    return {
-      result: 0,
-    };
-  },
-  methods: {
-    submit() {
-      //event.preventDefault();
-      this.$refs.child1.submit();
-      this.$refs.child2.submit();
-      this.result = this.$refs.child1.result1 + this.$refs.child2.result2;
-      return this.result;
+  data(){
+    return{
+        result:0,
+      }
     },
-  },
-};
+  methods:{
+      submit(){
+        this.$refs.child1.submit();
+        this.$refs.child2.submit();
+        
+        this.result=this.$refs.child1.result1+this.$refs.child2.result2;
+        return this.result;
+      },
+
+    }
+}
 </script>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-
   -webkit-font-smoothing: antialiased;
-
   -moz-osx-font-smoothing: grayscale;
-
-  text-align: center;
-
+  text-align:center;
   color: #2c3e50;
-
-  margin-top: 60px;
+  margin:unset;
+  background-color:darkcyan;
+  
 }
 </style>
+
+
+
---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,78 @@
+<template>
+  <div id="app">
+    
+    <h2>Mantra's Fun Quiz</h2>
+    <br>
+    <h2>Answer the following questions</h2>
+    <div v-for="item in questions" :key="item.index">
+      {{ item.Question }}
+      <div v-for="answer in item.options" :key="answer.index">
+        <input type="radio" :value="answer" v-model="item.answer" />
+        {{ answer }}
+      </div>
+    </div>
+   
+  </div>
+</template>
+<script>
+export default {
+  name:'QuestionBox',
+ 
+  data() {
+    return {
+      result1:0,
+      
+
+      questions:[
+        {
+          "Question" : "1.what is vue js ?",
+          options:[ "frontend framework","backend framework"],
+          correct:"frontend framework",answer:"",
+
+          },
+          {
+            "Question" : "2.what is javascript ?",
+            options:["scripting  Language","High level Language"],
+             correct:"scripting language",answer:""
+             
+             },
+             {
+              "Question" : "3.what is java ?",
+              options :[ "High level Language","low level Language"],
+               correct:"High level language",answer:""
+               
+               },
+               {
+                "Question" : "4.which language is platform independent ?",
+                options :[ "javascropt","java"],
+                 correct:"java",answer:""
+                 
+                 }
+                 ],
+                
+              value:"",   
+        };
+    },
+    
+   
+      methods:{
+       submit(){
+        for(let i of this.questions){
+          if(i.answer===i.correct){
+          this.result1=this.result1+1;
+         }        
+          
+        }
+        return this.result1;
+      },
+     
+
+
+      }
+    }
+   
+
+
+
+                
+</script>
