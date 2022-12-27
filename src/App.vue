a92d32f15735e4ad9a7d9a84206ffa503700550f
Manisha-500apps <120446675+Manisha-500apps@users.noreply.github.com>
2022-12-27 14:35:32 +0530

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,40 @@
 <template>
   <div id="app">
-    <!--<QuestionBox msg="Welcome to Your QuestionBox"/>
-    <FillBlanks msg="Welcome to Your FillBlanks"/>-->
-    <QuestionBox msg="welcome to quiz" ref="child1"/>
-  <FillBlanks msg="Fill in the blanks" ref="child2"/>
-    <button @click="submit()" :class="btn-submit">submit</button>
-    <p>Result : {{ result }}</p>
-   
+    <QuestionBox msg="welcome to quiz" ref="child1" />
+    <FillBlanks msg="Fill in the blanks" ref="child2" />
+    <p>Total Marks : {{ result }}</p>
+    <button @click="submit" :class="btn - submit">submit</button>
   </div>
-  
 </template>
 
 <script>
-import FillBlanks from './components/FillBlanks.vue';
 //import HelloWorld from './components/HelloWorld.vue'
-import QuestionBox from './components/QuestionBox.vue';
+import FillBlanks from "./components/FillBlanks.vue";
+//import HelloWorld from './components/HelloWorld.vue'
+import QuestionBox from "./components/QuestionBox.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-   
     QuestionBox,
-    FillBlanks
+    FillBlanks,
   },
-  data(){
-    return{
-        result:0,
-      }
-    },
-  methods:{
-  this.result=0;
-      submit(){
-        this.$refs.child1.submit();
-        this.$refs.child2.submit();
-        
-        this.result=this.$refs.child1.result1+this.$refs.child2.result2;
-        return this.result;
-      },
+  data() {
+    return {
+      result: 0,
+    };
+  },
+  methods: {
+    submit() {
+      this.result = 0;
+      this.$refs.child1.sum1();
+      this.$refs.child2.sum2();
 
-    }
-}
+      this.result = this.$refs.child1.result1 + this.$refs.child2.result2;
+      return this.result;
+    },
+  },
+};
 </script>
 
 <style>
@@ -47,11 +42,10 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align:center;
+  text-align: center;
   color: #2c3e50;
-  margin:unset;
-  background-color:darkcyan;
-  
+  margin-top: 60px;
+  background-color: darkcyan;
 }
 </style>
 
