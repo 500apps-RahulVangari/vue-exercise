3241082d854ad185e19e21181beb1c7aca98c6cf
500apps-sivamani <120447407+500apps-sivamani@users.noreply.github.com>
2023-01-02 13:07:37 +0530

    Update Heading-file.vue

--- src/components/Heading-file.vue
+++ src/components/Heading-file.vue
@@ -1,7 +1,9 @@
 <template>
-    <div>
-        <h1>This is the Home File </h1>
-        <p>Here There are 4 MCQ's and 4 Fill in the Blanks</p>
+    <div class="bg-home">
+        <h1 class="head">This is the Home File </h1>
+        <p class="para">Here There are 4 MCQ's and 4 Fill in the Blanks</p>
+        <p class="para1">There are 4 MCQ's in the MCQ Tab section <br/>For Correct Answer it will Add +1 otherwise -1</p>
+        <p class="para1">There are 4 Fill in the Blank's in the FB Tab section <br/>For Correct Answer it will Add +1 otherwise -1</p>
     </div>
 </template>
 
@@ -14,5 +16,26 @@ export default {
 </script>
 
 <style>
-
+    .head{
+        padding:20px;
+        font-weight:bold;
+        font-size: 30px;
+        font-family: "Roboto";
+        color:white;
+    }
+    .para{
+        color:white;
+        font-weight: bold;
+        font-size: 20px;
+    }
+    .bg-home{
+        background-color: rgb(111, 118, 123);
+        height:30vh;
+        background-size:cover;
+        margin:0px;
+    }
+    .para1{
+        color:rgb(181, 181, 89);
+        font-weight: bold;
+    }
 </style>
