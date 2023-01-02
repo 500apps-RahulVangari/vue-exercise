01d05cc397facfaa9b3f21f5ba6fc616b37afa85
500apps-sivamani <120447407+500apps-sivamani@users.noreply.github.com>
2023-01-02 10:12:16 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,45 +1,39 @@
 import Vue from 'vue'
-import App from './App.vue'
 import VueRouter from 'vue-router'
 
-import FB from './components/child-one.vue'
-import MCQ from './components/child-two.vue'
+import App from '../App.vue'
+import MCQs from '../components/child-one.vue'
+import FB from '../components/child-two.vue'
+import Result from '../components/Result-total.vue'
 
-Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+Vue.use(VueRouter)
 
 const routes = [
   {
-      path: '/fillintheblanks',
-      name: 'FB',
-      component: FB
+    path: '/',
+    name: 'App',
+    component: App
   },
-  //  Added our new route file named profile.vue
   {
-      path: '/mcq',
-      name: 'MCQ',
-      Component:MCQ
+    path: '/multipleChoiceQuestions',
+    name: 'MCQs',
+    component:MCQs
+  },
+  {
+    path: '/fillInTheBlanks',
+    name: 'FB',
+    component:FB
+  },
+  {
+    path:'/resultsView',
+    name:'Result',
+    component:Result
 
-  },
-  {
-      path: '/fillintheblanks',
-      name: 'FB',
-      // The route level code-splitting
-      // this generates a separate chunk 
-      // (about.[hash].js) for this route
-      // which is lazy-loaded when the
-      // route is visited.
-      component:()=> import(
-          /* webpackChunkName: "about" */ 
-          './App.vue'
-      )
-  },
+  }
+  
 ]
 
-// Create Vue Router Object
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
