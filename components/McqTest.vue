d9f2b2ac9ab0656611e43fa6432c307845246eb8
lakshmi500-apps <lakshmi.pilli@500apps.com>
2023-01-02 15:43:40 +0530

    updated quizapp

---
+++ components/McqTest.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="mcqs">
+    <h1>QUIZ</h1>
+    <h1>Multiple choice Questions</h1>
+    <hr />
+    <totalQns/>
+    <div v-for="data in questions" :key="data.id">
+      <p>{{ data.qns}}</p>
+      <div v-for="userAns in data.options" :key="userAns.id">
+        <input type="radio" :value="userAns" v-model="data.userAns" />{{ userAns }}
+        
+      </div>
+    </div>
+    <!-- <b-button @click="$router.push({path: '/BlanksTest'})">Next</b-button> -->
+    <b-button to="/BlanksTest">Next </b-button>
+  </div>
+
+   
+</template>
+<script>
+import totalQns from './json';
+    export default {
+      components: { totalQns },
+    name: 'McqTest',
+   // props:{questions:Array,},
+    
+    data(){
+        return{
+          res1:0,
+          questions:totalQns[0],
+          value:""
+       
+        }
+    },
+    methods:{
+        finalOutput(){
+            this.res1=0;
+            this.questions.forEach((item)=>{
+             if( item.userAns===item.correctAns )  {
+        
+                this.res1=this.res1+1;
+
+            }
+                return this.res1;
+        })
+       }
+    }
+};
+</script>
---
+++ components/resultPage.vue
@@ -0,0 +1,63 @@
+<template>
+    
+    <div><button @click="finalOutput">score</button> 
+    <h1>Your score is  {{ score }}</h1>
+    <b-button variant=" danger" ><router-link to='/'>Refresh</router-link></b-button>
+
+
+
+    <!-- <b-button to="/App">Reset </b-button> -->
+        <div id="name">
+           
+    <div>
+<McqTest ref ="child1"/>
+<BlanksTest ref ="child2"/></div>
+
+</div>
+</div>
+</template>
+<script>
+import BlanksTest from './BlanksTest.vue';
+import McqTest from './McqTest.vue';
+
+export default{
+    name:'resultPage',
+    components:{
+    McqTest,
+    BlanksTest,
+    
+    },
+    data(){
+        return{
+            score:0,
+        };
+    },
+
+    methods:{
+        finalOutput() {
+            // console.log('hello--------')
+            this.score=0;
+            this.$refs.child1.finalOutput();
+            this.$refs.child2.finalOutput();
+            this.score = this.$refs.child1.res1 + this.$refs.child2.res2;
+        return this.score
+        },
+    },
+
+   reset()
+  {
+    for (let i of this.totalQns){
+        console.log("hiiiiiii")
+      i.userAns=null
+    }
+    this.score=0
+  }
+    
+}
+</script>
+<style>
+#name{
+    visibility: hidden;
+}
+
+</style>
