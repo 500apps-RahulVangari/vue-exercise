a3fff3b4781fd4af560a1b13ad37cb37e618363c
Hariom Lohare <hariom.lohare@500apps.com>
2023-01-05 12:20:25 +0530

    Add documentation

--- src/components/Parent.vue
+++ src/components/Parent.vue
@@ -3,8 +3,9 @@
    
     <p id="b">
       <b>Marks distribution</b><br>
-      Maltiple Choice Question has one mak per question<br>
-      Fill in The Blanks has two marks per question</p>
+      Maltiple Choice Question has one mark per question<br>
+      Fill in The Blanks has two marks per question<br>
+      Malti Select  Question has three marks per question</p>
   </div>
 </template>
 
